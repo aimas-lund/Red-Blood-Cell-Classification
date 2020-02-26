864020cb4bd69e3c7b0acb81d0a8d90bf7b26502
Aimas Lund <aimas.lund@gmail.com>
2020-02-26 14:55:41 +0100

    Added image to array conversion

---
+++ data_handling.py
@@ -0,0 +1,38 @@
+import pickle as pkl
+import numpy as np
+from PIL import Image
+import glob
+import os
+
+
+def pickle_image(path, image, protocol=0):
+    file = open(path, 'wb')
+    pkl.dump(image, file)
+    file.close()
+
+
+def pickle_images(images, path='./data.pkl', protocol=3):
+    with open(path, 'wb') as file:
+        pkl.dump(images, file, protocol=protocol)
+    file.close()
+
+
+def images_to_array_list(path, file_extension='png', flatten=False):
+    """
+    Loads all images of a specified file-type (png by default) in a directory to a list of Numpy arrays
+    :param flatten: Optionally compresses the array into a single dimension
+    :param path: Path to directory containing images
+    :param file_extension: File extension to search for.
+    :return: List of Numpy arrays
+    """
+    output = []
+
+    for filename in glob.glob(os.path.join(path, '*.' + file_extension)):
+        x = np.asarray(Image.open(filename))
+
+        if flatten:
+            x = x.flatten()
+
+        output.append(x)
+
+    return output
