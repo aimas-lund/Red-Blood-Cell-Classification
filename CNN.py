51869b6d56d36b108a6fdb36f4cd66e183a2b964
Aimas Lund <aimas.lund@gmail.com>
2020-02-26 17:04:00 +0100

    Added additional layer types to CNN structure + auxiliary functions

--- CNN.py
+++ CNN.py
@@ -18,8 +18,15 @@ class CNN:
         self.network = None
         self.model = None
         self.layer_overview = []
+        self.layer_dict = self.init_layer_dict()
 
-    def pre_processing(self):   # normalizes the training data
+    def init_layer_dict(self):
+        keys = list(range(3))
+        layer_type = ["Convolutional Layer", "Fully Connected Layer", "Max Pooling Layer"]
+
+        return dict(zip(keys, layer_type))
+
+    def pre_processing(self):  # normalizes the training data
         image_prep = ImagePreprocessing()
         image_prep.add_featurewise_zero_center()
         image_prep.add_featurewise_stdnorm()
@@ -30,21 +37,27 @@ class CNN:
                      flip_left_right=True,
                      random_rotation=True,
                      random_blur=True):
-        image_aug = ImageAugmentation()
-        if flip_left_right:
-            image_aug.add_random_flip_leftright()  # adds left- and right flipped images to the training data
-
-        if random_rotation:
-            image_aug.add_random_rotation(max_angle=max_angle)  # rotates random training data by a specified angle (
-        # degrees)
-
-        if random_blur:
-            image_aug.add_random_blur(sigma_max=sigma_max)  # blurs random training data by a specified sigma
 
         if any([flip_left_right, random_blur, random_rotation]):
-            self.data_augmentation = image_aug
+            pass
         else:
-            print("No augmentation selected")
+            raise ValueError
+
+        try:
+            image_aug = ImageAugmentation()
+            if flip_left_right:
+                image_aug.add_random_flip_leftright()  # adds left- and right flipped images to the training data
+
+            if random_rotation:
+                image_aug.add_random_rotation(max_angle=max_angle)  # rotates random training data by a specified angle (
+            # degrees)
+
+            if random_blur:
+                image_aug.add_random_blur(sigma_max=sigma_max)  # blurs random training data by a specified sigma
+            self.data_augmentation = image_aug
+
+        except ValueError:
+            print("No augmentation selected!")
 
     def get_training_data(self, path):
         self.training_data = images_to_array_list(path)
@@ -60,7 +73,7 @@ class CNN:
         Initializes the network attribute for the CNN class
         :param shape: (list(int)) A list of integers specifying batch size, height-, width and depth of the input array,
          respectively.
-        :param name: (string) Given name to the network. Will be called 'InputData' by default.
+        :param name: (str) Given name to the network. Will be called 'InputData' by default.
         :return: An initialized network attribute for the CNN class.
         """
         if shape is None:
@@ -72,13 +85,81 @@ class CNN:
                                   name=name)
         self.layer_overview = [shape]
 
-    def network_add_conv_layer(self, activation='relu'):
+    def network_add_cl(self, nb_filter, filter_size,
+                       activation='relu',
+                       padding='same'):
         """
-        :param activation: (string) Specifying which type of activation should be applied to the weights in the given
-        layer. Default is set to 'rely' (Rectified Linear Unit).
-        :return: An added layer to the network attribute in the CNN class.
+        Adds a convolutional layer to the specified network.
+        :param nb_filter: (int) Specifying the number of convolutional filters
+        :param filter_size: (int or list(int)) Specifying the size of the filters
+        :param activation: (str) Specifying which type of activation function should be applied to the weights in the
+         given layer. Default is set to 'rely' (Rectified Linear Unit).
+        :param padding: (str) Specifying padding algorithm ('same' or 'valid')
+        :return: An added convolutional layer to the network attribute in the CNN class.
         """
         if self.network is None:
             print("Network not initialized. Initialize network first using the 'init_network' method!")
         else:
-            self.network = conv_2d(self.network, activation=activation)
+            self.network = conv_2d(self.network, nb_filter, filter_size,
+                                   activation=activation, padding=padding)
+            self.layer_overview = self.layer_overview.append(
+                [0, nb_filter, filter_size, activation, padding]
+            )  # appends a list of specifying values of the convolutional layer to the overview list
+
+    def network_add_fcl(self, n_units, activation='relu'):
+        """
+        Adds a fully connected layer to the specified network.
+        :param n_units: (int) Number of units of the layer
+        :param activation: (str) Specifying which type of activation function should be applied to the weights in the
+         given layer. Default is set to 'rely' (Rectified Linear Unit).
+        :return: An added fully connected layer to the network attribute in the CNN class.
+        """
+        if self.network is None:
+            print("Network not initialized. Initialize network first using the 'init_network' method!")
+        else:
+            self.network = fully_connected(self.network, n_units, activation=activation)
+            self.layer_overview = self.layer_overview.append(
+                [1, n_units, activation]
+            )  # appends a list of specifying values of the fully connected layer to the overview list
+
+    def network_add_mpl(self, kernel_size, padding='same'):
+        """
+        Adds a max pooling layer to the specified network.
+        :param kernel_size: (int) Pooling kernel size
+        :param padding: (str) Specifying padding algorithm ('same' or 'valid')
+        :return: An added max pooling layer to the network attribute in the CNN class.
+        """
+        if self.network is None:
+            print("Network not initialized. Initialize network first using the 'init_network' method!")
+        else:
+            self.network = max_pool_2d(self.network, kernel_size, padding=padding)
+            self.layer_overview = self.layer_overview.append(
+                [2, kernel_size, padding]
+            )  # appends a list of specifying values of the max pooling layer to the overview list
+
+    def fit_model(self):
+        # TODO: Fill this method out
+        x = 1
+
+
+    def save_model(self, filename='cnn_model'):
+        if self.model is None:
+            print("There is no model to save!")
+        else:
+            self.model.save(filename + ".tfl")
+            print("Model successfully saves as: " + filename + ".tfl")
+
+    def _display_model(self):
+
+        layers = self.layer_overview[:1]
+        if self.layer_overview[0][0] is None:
+            batch_number = 'Not specified'
+        else:
+            batch_number = str(self.layer_overview[0][0])
+
+        print("\n------------------------------------------")
+        print("Deep Neural Network Overview")
+        print("")
+
+        for layer in layers:
+            print("")   # TODO: Fix the printing function
